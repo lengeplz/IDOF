f7983d1a0257969578b3f040d25376784f9d4865
Andre <s3953146@student.rmit.edu.au>
2026-05-18 21:45:00 +1000

    init mainm

---
+++ main.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class item_stats:
+    asset_worth: float
+    aff_life: int
+    perc_work: float
+    year_bought: int
+    days_held: int
+
+
+def main():
+
+    while True:
+        asset_worth = float(input("Enter Value of item"))   
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
