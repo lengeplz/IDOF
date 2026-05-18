11e0538bdb44aaa2e27c140917c15c6501f80ae3
Andre <s3953146@student.rmit.edu.au>
2026-05-18 21:49:45 +1000

    updates to main()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from calc_logic import dim_val_met
 
 @dataclass
 class item_stats:
@@ -12,7 +13,17 @@ class item_stats:
 def main():
 
     while True:
-        asset_worth = float(input("Enter Value of item"))   
+        asset_worth = float(input("\nEnter Value of item: "))
+        aff_life = int(input("\nEnter the lifespan: "))
+        perc_work = float(input("\nEnter (%) used for work: "))
+        perc_work = perc_work / 100
+        year_bought = int(input("\n Year bought: "))
+        days_held = int(input("\n How many days held: "))
+
+        print("\n\nCalculating asset / time...")
+
+
+
 
     
 
